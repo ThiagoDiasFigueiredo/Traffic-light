18f66aa7f045ff53f5fb7fd806bd9fdec50c642b
ThiagoDiasFigueiredo <thiagodfigueiredo12@gmail.com>
2026-05-27 20:05:45 -0300

    Update traffic_light.py

--- traffic_light.py
+++ traffic_light.py
@@ -2,19 +2,20 @@ import time
 
 while True:
     contador = 25
+    
     for tempo in range(contador):
 
         if tempo == 0:
+            print('\033[0;31mRed')
             time.sleep(1)
-            print('\033[0;31mVermelho')
 
         elif tempo == 11:
+            print('\033[0;33mYellow')
             time.sleep(1)
-            print('\033[0;33mAmarelo')
 
         elif tempo == 14:
+            print('\033[0;32mGreen')
             time.sleep(1)
-            print('\033[0;32mVerde')
             
         print(tempo)
         tempo += 1
